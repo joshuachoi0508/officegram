f90d466ccaaa9e161cf921d1bb060fcc5c751190
Josh Choi <joshuachoi0508@gmail.com>
2018-12-11 11:56:39 -0800

    Download packages, make config file, and a basic static page

---
+++ frontend/officegram.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  ReactDOM.render(
+    <h1>OfficeGram TO BE MOUNTED SOON</h1>,
+    root)
+})
