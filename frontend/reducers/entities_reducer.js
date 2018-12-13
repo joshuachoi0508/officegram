d815449ca28530c03485931c6b238cda59157f43
Josh Choi <joshuachoi0508@gmail.com>
2018-12-13 15:21:46 -0800

    Add images reducer and add it to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
+import imagesReducer from './images_reducer';
 
 const entitiesReducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  images: imagesReducer
 });
 
 export default entitiesReducer;
---
+++ frontend/reducers/images_reducer.js
@@ -0,0 +1,23 @@
+import {
+  RECEIVE_ALL_IMAGES,
+  RECEIVE_ONE_IMAGE,
+  REMOVE_IMAGE
+} from '../actions/image_actions';
+
+const imagesReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch(action.type){
+    case RECEIVE_ALL_IMAGES:
+      return Object.assign({}, {action.images});
+    case RECEIVE_ONE_IMAGE:
+      return Object.assign({}, oldState, {[action.image.id]: action.image});
+    case REMOVE_IMAGE:
+      let newState = Object.assign({}, oldState);
+      delete newState[action.imageId];
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default imagesReducer;
