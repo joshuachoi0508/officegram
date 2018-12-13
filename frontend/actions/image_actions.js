6b64f570b95ca226cf7502602a8785abbf800498
Josh Choi <joshuachoi0508@gmail.com>
2018-12-13 14:01:05 -0800

    Add active storage methods in image api utils commented out

---
+++ frontend/actions/image_actions.js
@@ -0,0 +1,5 @@
+import * as ImageAPIUtil from '../util/image_api_util';
+
+export const RECEIVE_ALL_IMAGE = 'RECEIVE_ALL_IMAGE';
+export const RECEIVE_ONE_IMAGE = 'RECEIVE_ONE_IMAGE';
+export const REMOVE_IMAGE = 'REMOVE_IMAGE'
--- frontend/util/image_api_util.js
+++ frontend/util/image_api_util.js
@@ -41,3 +41,61 @@ export const fetchUserImages = userId => ({
     url: `api/users/${userId}/images`
   })
 });
+
+// adding file
+// he did this in app.jsx
+//
+// this.state = {
+//   posts: []
+// }
+//
+//
+// form.jsx
+//
+// this.state = {
+//   body: "",
+//   userId: "",
+//   photoFile: null,
+//   imageUrl: ""
+// }
+//
+// handleFile(e) {
+//   const file = e.currentTarget.files[0];
+//
+//   const fileReader = new FileReader();
+//   fileReader.onloadend = () = {
+//     this.setState({
+//       photoFile: file,
+//       imageUrl: fileReader.result
+//     });
+//   }
+//
+//   if (file) {
+//     fileReader.readAsDataURL(file);
+//   }
+// }
+//
+// handleSubmit(e) {
+//   e.preventDefault();
+//
+//   const formData = new FormData();
+//   formData.append('image[body]', this.state.body);
+//   if (this.state.photoFile) {
+//
+//     formData.append('image[image_url]', this.state.photoFile);
+//   }
+//   $.ajax({
+//     url: '/api/images',
+//     method: 'POST',
+//     data: formData,
+//     contentType: false,
+//     processData: false
+//   });
+// }
+//
+//
+// <form onSubmit={this.handleSubmit.bind(this)}>
+//   <input type="file"
+//     onChange={this.handleFile.bind.(this)}
+//   />
+// </form>
