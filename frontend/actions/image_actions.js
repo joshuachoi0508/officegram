75c0fde637a39fb14464ad6fb89db55d9323fd9f
Josh Choi <joshuachoi0508@gmail.com>
2018-12-13 15:31:18 -0800

    Add fetchUserImages action

--- frontend/actions/image_actions.js
+++ frontend/actions/image_actions.js
@@ -3,6 +3,7 @@ import * as ImageAPIUtil from '../util/image_api_util';
 export const RECEIVE_ALL_IMAGES = 'RECEIVE_ALL_IMAGES';
 export const RECEIVE_ONE_IMAGE = 'RECEIVE_ONE_IMAGE';
 export const REMOVE_IMAGE = 'REMOVE_IMAGE'
+export const RECEIVE_USER_IMAGES = 'RECEIVE_USER_IMAGES';
 
 const receiveAllImages = images => ({
   action: RECEIVE_ALL_IMAGES,
@@ -18,6 +19,11 @@ const removeImage = imageId => ({
   action: REMOVE_IMAGE,
   imageId: imageId
 })
+
+const receiveUserImages = images => ({
+  action: RECEIVE_USER_IMAGES,
+  images: images
+});
 
 export const fetchImages = () => dispatch => (
   ImageAPIUtil.fetchImages()
@@ -43,3 +49,8 @@ export const deleteImage = imageId => dispatch => (
   ImageAPIUtil.deleteImage(imageId)
     .then(imageId => dispatch(removeImage(imageId)))
 )
+
+export const fetchUserImages = userId => (
+  ImagesAPIUtil.fetchUserImages(userId)
+    .then(images => dispatch(images => receiveUserImages(images)))
+);
