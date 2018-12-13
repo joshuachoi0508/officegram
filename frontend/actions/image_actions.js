8be6fa9949f7278b12f2986e97c461bde481c696
Josh Choi <joshuachoi0508@gmail.com>
2018-12-13 14:54:31 -0800

    for heroku

--- frontend/actions/image_actions.js
+++ frontend/actions/image_actions.js
@@ -1,5 +1,17 @@
-import * as ImageAPIUtil from '../util/image_api_util';
-
-export const RECEIVE_ALL_IMAGE = 'RECEIVE_ALL_IMAGE';
-export const RECEIVE_ONE_IMAGE = 'RECEIVE_ONE_IMAGE';
-export const REMOVE_IMAGE = 'REMOVE_IMAGE'
+// import * as ImageAPIUtil from '../util/image_api_util';
+//
+// export const RECEIVE_ALL_IMAGES = 'RECEIVE_ALL_IMAGES';
+// export const RECEIVE_ONE_IMAGE = 'RECEIVE_ONE_IMAGE';
+// export const REMOVE_IMAGE = 'REMOVE_IMAGE'
+//
+// const receiveAllImages => images => ({
+//   action: RECEIVE_ALL_IMAGES,
+//   images: images
+// })
+//
+// export fetchImages = () => dispatch => (
+//   ImageAPIUtil.fetchImages()
+//     .then(images => dispatch(receiveAllImages(images)))
+// );
+//
+// export fetchImage
