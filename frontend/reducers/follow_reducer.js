fcd05e14eb55f4c4817f7f38728e356d6ba867c1
Josh Choi <joshuachoi0508@gmail.com>
2018-12-17 20:22:58 -0800

    Add follows reducer

---
+++ frontend/reducers/follow_reducer.js
@@ -0,0 +1,18 @@
+import {
+  RECEIVE_FOLLOW,
+  REMOVE_FOLLOW
+} from '../actions/follow_actions';
+
+const followsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch(action.type){
+    case RECEIVE_FOLLOW:
+      return Object.assign({}, oldState, {[action.follow.id]: action.follow});
+    case REMOVE_FOLLOW:
+      let newState = Object.assign({}, oldState);
+      delete newState[action.follow.id];
+      return newState;
+    default:
+      return oldState;
+  }
+};
