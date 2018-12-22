8384d4f0335fa2a4da7fddb3a0d2f3dfc124cdf4
Josh Choi <joshuachoi0508@gmail.com>
2018-12-21 17:06:16 -0800

    Add back local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-
   config.active_storage.service = :amazon_dev
 
   # Don't care if the mailer can't send.
