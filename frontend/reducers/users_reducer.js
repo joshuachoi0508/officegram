0bb34272fa9fd43775cd528e2e0520dd243db3cb
Josh Choi <joshuachoi0508@gmail.com>
2018-12-30 16:34:40 -0800

    Index like fixed

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -39,10 +39,14 @@ const usersReducer = (state = {}, action) => {
     case RECEIVE_USER:
       return Object.assign({}, state, { [action.user.id]: action.user })
     case RECEIVE_LIKE:
-      nextState[action.like.receiver.id].images[action.like.imageId].likerIds.push(action.like.userId)
+      if ([action.like.receiver.id].images) {
+        nextState[action.like.receiver.id].images[action.like.imageId].likerIds.push(action.like.userId)
+      }
       return nextState;
     case REMOVE_LIKE:
-      nextState[action.like.receiver.id].images[action.like.imageId].likerIds = nextState[action.like.receiver.id].images[action.like.imageId].likerIds.filter(likerId => likerId !== action.like.userId)
+      if ([action.like.receiver.id].images) {
+        nextState[action.like.receiver.id].images[action.like.imageId].likerIds = nextState[action.like.receiver.id].images[action.like.imageId].likerIds.filter(likerId => likerId !== action.like.userId)
+      }
       return nextState;
     default:
       return state;
