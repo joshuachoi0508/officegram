03e0b55c7d5e7adccb98c1adf48d99846b589ac4
Josh Choi <joshuachoi0508@gmail.com>
2019-01-30 13:16:03 -0800

    Logout out of App.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
-//testing
-import { logout } from '../actions/session_actions'
-
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 import ProfileContainer from './profile/profile_container';
