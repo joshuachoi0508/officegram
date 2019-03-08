5046b4682f802a97007e8e30bbb00f573ca42cde
Josh Choi <joshuachoi0508@gmail.com>
2019-03-07 16:10:01 -0800

    Delete unnecessary comments

--- Gemfile
+++ Gemfile
@@ -26,7 +26,6 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jquery-rails'
 gem 'rails_12factor'
-
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,8 +3,6 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-//dispatch prop just for testing delete later
-
 const Root = ({store}) => (
   <Provider store={store}>
     <HashRouter>
